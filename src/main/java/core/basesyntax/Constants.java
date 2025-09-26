0cb79bd1a5ce98de9987413560d54e296daa647f
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-09-26 10:04:20 +0200

    line of 107 in constants shouln't break app build

--- src/main/java/core/basesyntax/Constants.java
+++ src/main/java/core/basesyntax/Constants.java
@@ -3,5 +3,6 @@ package core.basesyntax;
 public class Constants {
     public static final int MIN_PASSWORD_LENGTH = 10;
     public static final String WRONG_PASSWORD_MESSAGE_VALIDATOR = "Wrong passwords";
-    public static final String WRONG_PASSWORD_MESSAGE_USER_SERVICE = "Your passwords are incorrect. Try again.";
+    public static final String WRONG_PASSWORD_MESSAGE_USER_SERVICE
+            = "Your passwords are incorrect. Try again.";
 }
